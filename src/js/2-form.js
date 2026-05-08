d5bccba25d16e287df022cd96f14df5b1d6a37fc
Anton-Symonenko <gan4yrnuk24@gmail.com>
2026-05-08 21:40:43 +0200

    done task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,65 @@
-console.log("hello");
+const formData = {
+    email: "",
+    message: ""
+};
+
+const Storage_Key = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+
+const saveData = loadFrom(Storage_Key);
+
+if (saveData) {
+  formData.email = saveData.email || "";
+  formData.message = saveData.message || "";
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener('input', e => {
+        formData.email = form.elements.email.value;
+        formData.message = form.elements.message.value;
+    
+    saveToLocalStorage(Storage_Key, formData);
+});
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+     if (!formData.email || !formData.message) {
+         alert("Fill please all fields");
+         return;
+     }
+    console.log(formData);
+
+    localStorage.removeItem(Storage_Key);
+        formData.email = "";
+        formData.message = "";
+    
+     form.reset();
+});
+
+
+function loadFrom(key) {
+  const jsonData = localStorage.getItem(key);
+   try {
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.log("error");
+    return null;
+   }
+}
+
+function saveToLocalStorage(key, value) {
+  const saveJson = JSON.stringify(value);
+  localStorage.setItem(key, saveJson);
+}
+
+
+
+
+
+
+
